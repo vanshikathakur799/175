f5f5f20fd8a186f506f7d69abea9695db0f656a1
Vanshika Thakur <103041940+vanshikathakur799@users.noreply.github.com>
2023-12-17 18:51:37 +0530

    Add files via upload

---
+++ createModel.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent('models', {
+  init: async function () {
+
+    var models = await this.getModels()
+    var barcodes = Object.keys(models)
+
+    barcodes.map(barcode => {
+      var model = models[barcode]
+      this.createModels(model)
+    })
+  },
+
+  getModels: function () {
+    return fetch("model.json")
+      .then(res => res.json())
+      .then(data => data)
+  },
+
+  createModels: async function (model) {
+    var modelName = model.model_name
+    var barcodeValue = model.barcode_value
+    var modelUrl = model.model_url
+
+    var scene = document.querySelector('a-scene')
+
+    var marker = document.createElement('a-marker')
+
+    marker.setAttribute('id', `marker-${modelName}`)
+    marker.setAttribute('type', 'barcode')
+    marker.setAttribute('model_name', modelName)
+    marker.setAttribute('value', barcodeValue)
+    marker.setAttribute('markerhandler', {})
+    scene.appendChild(marker)
+
+    if(barcodeValue === 0){
+      var modelEl = document.createElement('a-entity')
+      modelEl.setAttribute('id', `${modelName}`)
+      modelEl.setAttribute('geometry', {primitive: 'box', width: model.width, height: model.height})
+      modelEl.setAttribute('position', model.position)
+      modelEl.setAttribute('rotation', model.rotation)
+      modelEl.setAttribute('material', {color: model.color})
+      marker.appendChild(modelEl)
+    }
+    else{
+      var modelEl = document.createElement('a-entity')
+      modelEl.setAttribute('id', `${modelName}`)
+      modelEl.setAttribute('gltf-model', `url(${modelUrl})`)
+      modelEl.setAttribute('scale', model.scale)
+      modelEl.setAttribute('position', model.position)
+      modelEl.setAttribute('rotation', model.rotation)
+      marker.appendChild(modelEl)
+    }
+  }
+})
---
+++ markerHandler.js
@@ -0,0 +1,98 @@
+var modelList = []
+
+AFRAME.registerComponent('markerhandler', {
+    init: async function() {
+        this.el.addEventListener("markerFound", () => {
+            var modelName = this.el.getAttribute("model_name")
+            var barcodeValue = this.el.getAttribute("barcode_value")
+            modelsArray.push({model_name: modelName, barcode_value: barcodeValue})
+
+            var model = document.querySelector(`#${modelName}-${barcodeValue}`) 
+            model.setAttribute("visible", true)
+        })
+
+        this.el.addEventListener("markerLost", () => {
+            var modelName = this.el.getAttribute("model_name")
+            var index = modelsArray.findIndex(x => x.model_name === modelName)
+            if (index > -1) {
+              modelsArray.splice(index, 1)
+            }
+          })
+    },
+
+    getDistance: function (elA, elB) {
+        return elA.object3D.position.distanceTo(elB.object3D.position)
+      },
+
+      getModelGeometry: function(models, modelName) {
+        var barcodes = Object.keys(models)
+        for (var barcode of barcodes) {
+            if(models[barcode].model_name === modelName) {
+                return{
+                    position: models[barcode]["placement_position"],
+                    rotation: models[barcode]["placement_rotation"],
+                    scale: models[barcode]["placement_scale"],
+                    model_url: models[barcode]["model_url"]
+                }
+            }
+        }
+      },
+
+      isModelPresentArray: function(arr, val){
+        for (var i of arr) {
+            if(i.model_name === val) {
+                return true
+            }
+        }
+        return false
+      },
+
+      tick: async function() {
+        if(modelList.length > 1){
+            var isBaseModelPresent = this.isModelPresentArray(modelList, 'base')
+            var messageText = document.querySelector("#message-text")
+
+            if(!isBaseModelPresent) {
+                messageText.setAttribute("visible", true)
+            }
+            else{
+                if(models === null){
+                    models = await this.getModels()
+                }
+
+                messageText.setAttribute("visible", false)
+                this.placeTheModel("road", models)
+                this.placeTheModel("car", models)
+                this.placeTheModel("sun", models)
+                this.placeTheModel("tree_1", models)
+            }
+        }
+      },
+
+      placeTheModel: function(modelName, models) {
+        var isListContainModel = this.isModelPresentArray(modelList, modelName)
+        if(isListContainModel) {
+            var distance = null
+            var marker1 = document.querySelector(`#marker-base`)
+            var marker2 = document.querySelector(`#marker-${modelName}`)
+
+            distance = this.getDistance(marker1, marker2)
+            if(distance < 1.25){
+                var modelEl = document.querySelector(`#${modelName}`)
+                modelEl.setAttribute('visible', false)
+
+                var isModelPlaced = document.querySelector(`#model-${modelName}`)
+                if(isModelPlaced === null){
+                    var el = document.createElement('a-entity')
+                    var modelGeometry = this.getModelGeometry(models, modelName)
+                    el.setAttribute('id', `model-${modelName}`)
+                    el.setAttribute('gltf-model', `url(${modelGeometry.model_url})`)
+                    el.setAttribute('position', modelGeometry.position)
+                    el.setAttribute('rotation', modelGeometry.rotation)
+                    el.setAttribute('scale', modelGeometry.scale)
+                    marker1.appendChild(el)
+                }
+            }
+        }
+      }
+})
